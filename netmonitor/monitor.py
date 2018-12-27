f612c2929735868fd475df5a66ace7acb2ac7a7f
gsam1 <gsamardjiev@gmail.com>
2018-12-28 00:57:19 +0200

    Monitor able to ping hosts in config file. DB functionality not created

--- netmonitor/monitor.py
+++ netmonitor/monitor.py
@@ -2,14 +2,63 @@ from multiping import MultiPing
 import json
 
 
-HOSTS = json.load(open('./config/hosts.json'))['hosts']
-HOSTS_TO_PING = [HOSTS[key] for key in HOSTS.keys()]
 
-mp = MultiPing(HOSTS_TO_PING)
+class HostStatus:
+    def __init__(self):
+        '''
+            Initalize the class to get all of the needed options and hosts.
+        '''
+        self.hosts = json.load(open('./config/config.json'))['hosts']
+        self.hosts_addresses = [self.hosts[key] for key in self.hosts.keys()]
+        self.response_time = json.load(open('./config/config.json'))['options']['response-time']
 
-mp.send()
+    def _host_mapper(self, address, status):
+        '''
+            Maps the address to the hostname.
+        '''
+        for name, ip in self.hosts.items():
+            if ip == address:
+                hostname = name
 
-responses, no_responses = mp.receive(2)
+        return {'host': {'hostname': hostname, 'address':address, 'status':status}}
 
-print(responses)
-print(no_responses)
+    def hosts_status(self):
+        '''
+            Check all hosts in the hosts file.
+            Returns a hosts array with objects.
+        '''
+        mp = MultiPing(self.hosts_addresses)
+        mp.send()
+
+        responses, no_responses = mp.receive(self.response_time)
+
+        resp = [self._host_mapper(addr, 'up') for addr in responses]
+        no_resp = [self._host_mapper(addr, 'down') for addr in no_responses]
+
+        return {'overview': resp + no_resp,
+                'only_up': resp,
+                'only_down': no_resp}
+
+
+    def pretty_status(self):
+        '''
+            Invokes the host_status method.
+            Returns status by individual host and counts as a total.
+        '''
+        hosts_stat = self.hosts_status()
+        host_status = [host['host']['hostname'] + ' ' + host['host']['status'] for host in hosts_stat['overview']]
+        total_up = len(hosts_stat['only_up'])
+        total_down = len(hosts_stat['only_down'])
+
+        return host_status, total_up, total_down
+
+
+    def push_to_db(self):
+        '''
+            Upload the results of the hosts query.
+        '''
+        pass
+
+if __name__  == '__main__':
+    host_status = HostStatus()
+    print(host_status.pretty_status())
