38277ad6df6db948ac65f48fecca7823703a16e9
gsam1 <gsamardjiev@gmail.com>
2018-12-30 13:23:52 +0200

    dbhandler somewhat finished

--- db/dbhandler.py
+++ db/dbhandler.py
@@ -1,49 +1,97 @@
 import sys, os
 import sqlite3
-from sqlalchemy import create_engine, ForeignKey
-from sqlalchemy import Column, Date, Integer, String
+import datetime
+from sqlalchemy import create_engine
+from sqlalchemy import Column, Date, Integer, String, DateTime, func
+from sqlalchemy.orm import scoped_session,sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 
-class DBHandler(object):
+Base = declarative_base()
+
+class Status(Base):
+    __tablename__ = 'status'
+    id = Column(Integer, primary_key = True)
+    lastupdate = Column(DateTime, default = func.now(), onupdate='{:%Y-%m-%d %H:%M:%S}'.format(datetime.datetime.now()))
+    up = Column('up', Integer)
+    down = Column('down', Integer)
+
+
+class DBHandler():
     def __init__(self):
         folder = os.path.dirname(os.path.realpath(__file__))
         sqlconnection = 'sqlite:///' + folder + '/local.db'
         self.engine = create_engine(sqlconnection)
-        self.base = 
+
+    def _session(self):
+        Session = sessionmaker(bind = self.engine)
+        return Session()
     
-    def create_table(self, table, format):
-
-
-    def get_last(self, table, criteria):
- 
-
-
-    def get_one(self, criteria):
-        pass
+    def create_table(self, table):
+        if not self.engine.dialect.has_table(self.engine, table.__tablename__):
+            table.__table__.create(bind=self.engine, checkfirst=True)
+        
+        Base.metadata.create_all(self.engine)
     
-    def sort_table(self):
-        pass
-
-
-    def insert_one(self, table, columns, item):
-
+    def push_one(self, data):
+        '''
+            Data object must be a class of the sql alchemy.
+        '''
+        # session create
+        session = self._session()
+        # add the data
+        session.add(data)
+        # commit
+        session.commit()
+        # close
+        session.close()
     
-    def insert_many(self, table, columns, data):
+    def push_many(self, data_array):
+        for item in data_array:
+            self.push_one(item)
+    
+    def get_all(self, table):
+        session = self._session()
+        result = session.query(table)
 
+        response = []
+        for item in result:
+            response.append({
+                'lastupdate':'{:%Y-%m-%d %H:%M:%S}'.format(item.lastupdate),
+                'up': item.up,
+                'down': item.down
+            })
 
+        session.close()
+
+        return response
+
+    def get_last(self, table):
+        session = self._session()
+        result = session.query(table).order_by(table.lastupdate.desc()).limit(1)
+        
+        response = {}
+        for item in result:
+            response['lastupdate'] = '{:%Y-%m-%d %H:%M:%S}'.format(item.lastupdate)
+            response['up'] = item.up
+            response['down'] = item.down
+
+        session.close()
+
+        return response
+        
 if __name__ == '__main__':
+    dbhandler = DBHandler()
+    status_table = Status()
+    dbhandler.create_table(status_table)
 
-    # dbhandler = DBHandler()
-    
-    # table_format = 'id INTEGER PRIMARY KEY, tstamp TEXT, data TEXT'
-    # table = 'qwez'
-    # columns = 'tstamp, data'
-    # dbhandler.create_table(table, table_format)
-    # item1 = ("[timestamp1]","{'z':1}")
-    # item2 = ("[timestamp2]","{'a':2}")
-    # item3 = ("[timestamp3]","{'d':3}")
-    # datapush = [item1, item2]
+    # status = Status()
+    # status.up = 214
+    # status.down = 2
 
-    # dbhandler.insert_many(table, columns, datapush)
-    # dbhandler.insert_one(table, columns, item3)
+    # dbhandler.push_one(status)
+
+    last = dbhandler.get_last(Status)
+    print(last)
+    all_entries = dbhandler.get_all(Status)
+    print(all_entries)
