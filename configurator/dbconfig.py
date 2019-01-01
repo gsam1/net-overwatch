f762ff603f5551b1485a2c68463742bcd0ac6b03
gsam1 <gsamardjiev@gmail.com>
2019-01-01 21:09:18 +0200

    Configurator fix - wrong module load

--- configurator/dbconfig.py
+++ configurator/dbconfig.py
@@ -2,7 +2,7 @@ import os, sys
 import json
 db_location = json.load(open('../configurator/module_map.json'))['db']
 sys.path.append(db_location)
-import Status, DBHandler
+from dbhandler import Status, DBHandler
 
 
 def create_db_table():
