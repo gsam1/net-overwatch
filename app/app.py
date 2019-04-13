fd08d3e5544c07d79d4ba0744bfc173a752922b7
gsam1 <gsamardjiev@gmail.com>
2019-04-14 01:07:32 +0300

    Added 2 more routes to the http api

--- app/app.py
+++ app/app.py
@@ -41,9 +41,46 @@ def update_host():
     dbhandler.update_host(id=host_id, params_to_update=req['vars'])
 
     return f'Host {hostname} updated'
+                                                                                                                
+@app.route('/get_last_hosts_status', methods=['GET'])
+def get_last_hosts_status():
+    response = dbhandler.get_last_pushed_results()
+    timestamp = '{:%Y-%m-%d %H:%M:%S}'.format(response['timestamp'])
     
+    # format the response's timestamp
+    for item in response['details']:
+        item['timestamp'] = '{:%Y-%m-%d %H:%M:%S}'.format(item['timestamp'])
+    
+    resp = {
+        'timestamp': timestamp,
+        'summary': {
+            'up': response['up'],
+            'down': response['down']
+        },
+        'details': response['details']
+    }
 
+    return json.dumps(resp)
 
+@app.route('/get_current_hosts_status', methods=['GET'])
+def get_current_hosts_status():
+    host_status = HostStatus()
+    hosts_stat = host_status.hosts_status()
+    total_up = len(hosts_stat['only_up'])
+    total_down = len(hosts_stat['only_down'])
+    timestamp = '{:%Y-%m-%d %H:%M:%S}'.format(datetime.datetime.now())
+    details = [host['host'] for host in hosts_stat['overview']]
+
+    resp = {
+        'timestamp': timestamp,
+        'summary': {
+            'up': total_up,
+            'down': total_down
+        },
+        'details': details
+    }
+
+    return json.dumps(resp)
 
 if __name__ == '__main__':
     app.run(debug=True)
